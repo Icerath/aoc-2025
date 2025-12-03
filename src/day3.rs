c888efb505e505d0a450a1e74650eee7c7e004dc
Icerath <dorjegilfillan@gmail.com>
2025-12-03 15:07:11 +0200

    day3part2

--- src/day3.rs
+++ src/day3.rs
@@ -1,19 +1,42 @@
+use std::collections::HashMap;
+
+use rustc_hash::FxHashMap;
+
 const EXAMPLE: &str = "987654321111111
 811111111111119
 234234234234278
 818181911112111";
 
-fn part1(input: &str) -> u32 {
-    input.lines().map(highest_in_bank).sum()
+fn part1(input: &str) -> u64 {
+    input.lines().map(highest_in_bank(2)).sum()
 }
 
-fn highest_in_bank(bank: &str) -> u32 {
+fn part2(input: &str) -> u64 {
+    input.lines().map(highest_in_bank(12)).sum()
+}
+
+fn highest_in_bank(n: u32) -> impl Fn(&str) -> u64 {
+    move |bank| highest_n_in_bank(bank, n, &mut HashMap::default())
+}
+
+fn highest_n_in_bank<'a>(
+    bank: &'a str,
+    n: u32,
+    cache: &mut FxHashMap<(&'a [u8], u32), u64>,
+) -> u64 {
+    if let Some(cached) = cache.get(&(bank.as_bytes(), n)) {
+        return *cached;
+    }
+    if n == 0 {
+        return 0;
+    }
     let mut max = 0;
-    for start in 0..(bank.len() - 1) {
-        let a = (bank.as_bytes()[start] - b'0') as u32 * 10;
-        let b = bank[(start + 1)..].bytes().map(|b| b - b'0').max().unwrap() as u32;
+    for start in 0..(bank.len() - (n - 1) as usize) {
+        let a = (bank.as_bytes()[start] - b'0') as u64 * 10u64.pow(n - 1);
+        let b = highest_n_in_bank(&bank[start + 1..], n - 1, cache);
         max = (a + b).max(max)
     }
+    cache.insert((bank.as_bytes(), n), max);
     max
 }
 
@@ -25,4 +48,14 @@ fn test_part1_example() {
 #[test]
 fn test_part1() {
     assert_eq!(part1(include_str!("../input/day3.txt")), 17403);
+}
+
+#[test]
+fn test_part2_example() {
+    assert_eq!(part2(EXAMPLE), 3121910778619);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(include_str!("../input/day3.txt")), 173416889848394);
 }
