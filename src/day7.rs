8d821ca8617c724e4ede8396d4625487c52eed66
Icerath <dorjegilfillan@gmail.com>
2025-12-09 19:04:00 +0200

    day7 part2

--- src/day7.rs
+++ src/day7.rs
@@ -1,3 +1,4 @@
+use rustc_hash::FxHashMap;
 use std::collections::VecDeque;
 
 const EXAMPLE: &str = ".......S.......
@@ -42,6 +43,37 @@ fn part1(input: &str) -> u32 {
     sum
 }
 
+fn part2(input: &str) -> u64 {
+    let input = input.as_bytes();
+    let row_len = input.iter().position(|&b| b == b'\n').unwrap() + 1;
+    let index = input.iter().position(|&b| b == b'S').unwrap();
+    part2_inner(input, index, row_len, &mut FxHashMap::default()) + 1
+}
+
+fn part2_inner(
+    grid: &[u8],
+    current: usize,
+    row_len: usize,
+    cache: &mut FxHashMap<usize, u64>,
+) -> u64 {
+    if let Some(cached) = cache.get(&current) {
+        return *cached;
+    }
+    let next = current + row_len;
+
+    let mut sum = 0;
+    if next >= grid.len() {
+    } else if grid[next] == b'^' {
+        sum += 1;
+        sum += part2_inner(grid, next + 1, row_len, cache);
+        sum += part2_inner(grid, next - 1, row_len, cache);
+    } else {
+        sum += part2_inner(grid, next, row_len, cache);
+    }
+    cache.insert(current, sum);
+    sum
+}
+
 #[test]
 fn test_part1_example() {
     assert_eq!(part1(EXAMPLE), 21);
@@ -50,4 +82,14 @@ fn test_part1_example() {
 #[test]
 fn test_part1() {
     assert_eq!(part1(include_str!("../input/day7.txt")), 1638);
+}
+
+#[test]
+fn test_part2_example() {
+    assert_eq!(part2(EXAMPLE), 40);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(include_str!("../input/day7.txt")), 7759107121385);
 }
