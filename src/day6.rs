ef9ee20fc79ef6ebe80d69cf5ba1c0f2156c1c57
Icerath <dorjegilfillan@gmail.com>
2025-12-09 18:00:30 +0200

    ugly day6 part2

--- src/day6.rs
+++ src/day6.rs
@@ -34,6 +34,41 @@ fn part1(input: &str) -> u64 {
     sum
 }
 
+fn part2(input: &str) -> u64 {
+    let (numbers, operators) = input.rsplit_once("\n").unwrap();
+
+    let operators =
+        operators.split_whitespace().map(|s| if s == "*" { Operator::Mul } else { Operator::Add });
+
+    let lines: Vec<_> = numbers.lines().collect();
+    assert_eq!(lines.iter().map(|s| s.len()).min(), lines.iter().map(|s| s.len()).max());
+
+    let mut current_pos = 0;
+    let mut cols: Vec<Vec<_>> = vec![];
+    while lines[0].len() > current_pos {
+        let largest = (lines.iter().map(|s| &s[current_pos..]))
+            .map(|s| s.find(' ').unwrap_or(s.len()))
+            .max()
+            .unwrap();
+        cols.push(lines.iter().map(|s| s[current_pos..current_pos + largest].as_bytes()).collect());
+        current_pos += largest + 1;
+    }
+
+    let mut sum = 0;
+    for (col, operator) in cols.iter().zip(operators) {
+        let iter = (0..col[0].len()).map(|digit| {
+            col.iter()
+                .flat_map(|s| (s[digit] != b' ').then(|| (s[digit] - b'0') as u64))
+                .fold(0, |acc, x| acc * 10 + x)
+        });
+        sum += match operator {
+            Operator::Add => iter.sum::<u64>(),
+            Operator::Mul => iter.product::<u64>(),
+        }
+    }
+    sum
+}
+
 #[test]
 fn test_part1_example() {
     assert_eq!(part1(EXAMPLE), 4277556);
@@ -42,4 +77,14 @@ fn test_part1_example() {
 #[test]
 fn test_part1() {
     assert_eq!(part1(include_str!("../input/day6.txt")), 5060053676136);
+}
+
+#[test]
+fn test_part2_example() {
+    assert_eq!(part2(EXAMPLE), 3263827);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(include_str!("../input/day6.txt")), 9695042567249);
 }
